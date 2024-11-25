23c265c855506ffc5b1e74035a83a776feefbca6
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-11-25 13:47:52 -0300

    implementando funciones

--- src/functions/post.php
+++ src/functions/post.php
@@ -15,16 +15,25 @@ function createPost($title, $content, $author_id)
 
 function getAllPosts()
 {
+    global $conexion;
+
+    $query = ""; // los de base  de datos me deben pasar el query para tener todos los posts
+    return mysqli_query($conexion, $query); // aca se devuelve la data
+
 }
 
-function getPostById()
+function getPostById($author_id)
 {
+    global $conexion;
+
+    $author_id = (int) $author_id; // se castea para que si o si sea un int
+    $query = ""; // aca los de base de datos me deben pasar el query y yo coloco el id del usuario
+
 }
 
 function updatePost()
 {
 }
-
 
 function deletePost()
 {
