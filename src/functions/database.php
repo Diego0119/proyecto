a59422e4c32063a4bbb42c4709ff89f4c0f8be39
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-12-04 14:04:21 -0300

    añadiendo archivo de prueba

--- src/functions/database.php
+++ src/functions/database.php
@@ -1,12 +1,14 @@
 <?php
-$databaseHost = 'localhost';
+$databaseHost = '127.0.0.1';
 $databaseName = 'tft';
 $databaseUsername = 'root';
 $databasePassword = '';
 
 $conexion = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-if (!$conexion) {
-    die("Error en la conexión: " . mysqli_connect_error());
+if ($conexion) {
+    echo "Conexión exitosa a la base de datos.";
+} else {
+    echo "Error en la conexión: " . mysqli_connect_error();
 }
 ?>
---
+++ src/functions/test_functions.php
@@ -0,0 +1,47 @@
+<?php
+// include('database.php');
+include('comps.php');
+
+
+// // $name_of_composition = "Composición de prueba";
+// // $difficulty = 5;
+// // $rating = 4.2;
+// // $champions = ["Ahri", "Yasuo", "Garen", "Zed", "Teemo", "Lux"];
+// // $items = ["Espada de Rúnica", "Cloak of Agility"];
+
+
+// // echo createComps($name_of_composition, $difficulty, $rating, $champions, $items);
+echo "<br><br>";
+$compositions = getAllComps();
+if (is_array($compositions)) {
+    echo "Composiciones obtenidas: <br>";
+    foreach ($compositions as $comp) {
+        echo "Composición: " . $comp['name'] . " - Dificultad: " . $comp['difficulty'] . " - Rating: " . $comp['rating'] . "<br>";
+    }
+} else {
+    echo $compositions;
+}
+echo "<br><br>";
+
+
+$champions = getAllChampions();
+if (is_array($champions)) {
+    echo "Campeones obtenidos: <br>";
+    foreach ($champions as $champion) {
+        echo "Campeón: " . $champion['name'] . "<br>";
+    }
+} else {
+    echo $champions;
+}
+echo "<br><br>";
+
+$items = getAllItems();
+if (is_array($items)) {
+    echo "Ítems obtenidos: <br>";
+    foreach ($items as $item) {
+        echo "Ítem: " . $item['name'] . "<br>";
+    }
+} else {
+    echo $items;
+}
+?>
