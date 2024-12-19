eceda01e5d324ee7617c93ffec2c24768f01519c
ChrissNMS <christofer.guti555@gmail.com>
2024-12-19 01:35:19 -0300

    Create database.php

---
+++ pages/database.php
@@ -0,0 +1,9 @@
+<?php include '../header.php'; ?>
+    <body class="bg-black text-white font-sans flex flex-col min-h-screen">
+     <p>
+        
+     </p>       
+        
+    </body>
+
+<?php include '../footer.php'; ?>
