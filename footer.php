c665a7e6ace606e1d9981d9a6b8b8684d439b606
ChrissNMS <christofer.guti555@gmail.com>
2024-12-19 01:41:31 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer class="bg-purple-900 text-white">
+  <div class="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-md">
+      <strong class="block text-center text-xl font-bold text-purple-300 sm:text-3xl">
+        ¿Quieres enterarte de las últimas noticias en TFT? Contáctanos
+      </strong>
+
+      <form class="mt-6">
+        <div class="relative max-w-lg">
+          <label class="sr-only" for="email"> Email </label>
+
+          <input
+            class="w-full rounded-full border-purple-600 bg-purple-700 p-4 pe-32 text-sm font-medium"
+            id="email"
+            type="email"
+            placeholder="tft@guia.cl"
+          />
+
+          <button
+            class="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-purple-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-purple-700"
+          >
+            Subscríbete
+          </button>
+        </div>
+      </form>
+    </div>
+
+    <div class="mt-16 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-32">
+      <div class="mx-auto max-w-sm lg:max-w-none">
+        <p class="mt-4 text-center text-purple-200 lg:text-left lg:text-lg">
+          ¡Bienvenido a la mejor página de builds de TFT! Aquí podrás encontrar guías actualizadas para mejorar tu juego y competir al más alto nivel.
+        </p>
+
+        <div class="mt-6 flex justify-center gap-4 lg:justify-start">
+        </div>
+      
+      </div>
+
+      <!--derechos de autor-->
+      <div class="text-center lg:text-left">
+        <p class="text-purple-300">© 2024 Builds de TFT. Todos los derechos reservados.</p>
+        <p class="text-purple-200 text-sm">Este sitio no está afiliado, patrocinado ni respaldado por Riot Games. TFT y League of Legends son marcas registradas de Riot Games, Inc.</p>
+        <div class="flex justify-center lg:justify-start mt-4">
+          <a href="https://www.riotgames.com/" target="_blank" rel="noopener noreferrer">
+            <img src="../img/logo-riot.webp" alt="Riot Games Logo" class="w-20 mt-2">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
