9afb11a13db22f39b570fbbab3241cc4271fdadc
ChrissNMS <christofer.guti555@gmail.com>
2024-12-19 01:32:47 -0300

    Update database.php
    Borre el mensaje que mostraba cuando se conectaba a la base de datos

--- src/functions/database.php
+++ src/functions/database.php
@@ -5,10 +5,4 @@ $databaseUsername = 'root';
 $databasePassword = '';
 
 $conexion = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
-if ($conexion) {
-    echo "Conexión exitosa a la base de datos.";
-} else {
-    echo "Error en la conexión: " . mysqli_connect_error();
-}
 ?>
